37d20a24c73959fe746b2c736d11a3621ec943d4
pb125 <pb9039172018@gmail.com>
2021-12-04 19:45:03 +0530

    javascript1

---
+++ js/get.js
@@ -0,0 +1,17 @@
+// document.write("Hello")
+// document.write("Hello");
+
+
+// var element;
+// element=document.body;
+
+// console.log(element)
+
+//element=document.getElementById("a").innerText;
+//element=document.getElementById("a").innerHTML;
+//element=document.getElementById("a").getAttribute("style");
+//element=document.getElementById("a").getAttributeNode("style");
+//element=document.getElementById("a").attributes[0];
+
+
+console.log(element);
